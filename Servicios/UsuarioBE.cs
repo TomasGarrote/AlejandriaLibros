aad6fa57b4c994656f5443f0fcd7af9ea4139b09
Ivan Paco <ivanpacochoque466@gmail.com>
2026-05-28 15:24:18 -0300

    Se agrego la Clase UsuarioBE para la gestion de usuario en la capa Servicio

---
+++ Servicios/UsuarioBE.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servicios
+{
+    public class UsuarioBE
+    {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Username { get; set; }
+        [Browsable(false)]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        [Browsable(false)]
+        public bool Bloqueado { get; set; }
+        [Browsable(false)]
+        public bool Activo { get; set; }
+        public string Rol { get; set; }
+
+        public UsuarioBE() { }
+
+        public UsuarioBE(string dNI, string nombre, string apellido, string nombreUsuario, string password, string email, bool bloqueado, bool activo, string nombre_rol)
+        {
+            DNI = dNI;
+            Nombre = nombre;
+            Apellido = apellido;
+            Username = nombreUsuario;
+            Password = password.Trim();
+            Email = email;
+            Bloqueado = bloqueado;
+            Activo = activo;
+            Rol = nombre_rol;
+        }
+    }
+}
