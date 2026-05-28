68aa969279833a29046fdc82baf5b75fa44807aa
TomasGarrote <tomasgarrote123@gmail.com>
2026-05-28 16:46:09 -0300

    Agrego singleton ManejadorDeSesion

---
+++ Servicios/ManagerDeSesion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servicios
+{
+    public class ManagerDeSesion
+    {
+        private static ManagerDeSesion _instance;
+        private string _usuarioLogueado;
+        private ManagerDeSesion()
+        {
+            _usuarioLogueado = null;
+        }
+        public static ManagerDeSesion Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ManagerDeSesion();
+                }
+                return _instance;
+            }
+        }
+        public void Loguear(string usuario)
+        {
+            if (Logueado())
+            {
+                throw new Exception("Ya hay un usuario logueado.");
+            }
+            _usuarioLogueado = usuario;
+        }
+        public void Desloguear()
+        {
+            if (!Logueado())
+            {
+                throw new Exception("No hay ningún usuario logueado.");
+            }
+            _usuarioLogueado = null;
+        }
+        public bool Logueado()
+        {
+            return _usuarioLogueado != null;
+        }
+        public string UsuarioActual()
+        {
+            if (!Logueado())
+            {
+                throw new Exception("No hay ningún usuario logueado.");
+            }
+            return _usuarioLogueado;
+        }
+    }
+}
