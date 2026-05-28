75f93c99498488bd0410671baa8e00c415fde6ab
Ivan Paco <ivanpacochoque466@gmail.com>
2026-05-28 15:21:06 -0300

    Se agrego clase abstractDAL para la conexion

---
+++ DAL/AbstractDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public abstract class AbstractDAL<T>where T : class
+    {
+        protected SqlConnection _sqlserver;
+        protected SqlCommand _sqlcommand;
+
+        protected AbstractDAL()
+        {
+            _sqlserver = new SqlConnection("Data Source=.;Initial Catalog=Alejandria_DB;Integrated Security=True;TrustServerCertificate=True");
+            _sqlcommand = new SqlCommand();
+            _sqlcommand.Connection = _sqlserver;
+            _sqlcommand.CommandType = System.Data.CommandType.Text;
+        }
+    }
+}
