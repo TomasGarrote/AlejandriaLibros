cdcd21fb71c8cfe3c2d4fbf2a1d64c52f7360241
TomasGarrote <tomasgarrote123@gmail.com>
2026-05-28 16:46:55 -0300

    Agrego UsuarioDAL

---
+++ DAL/UsuarioDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Servicios;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void BloquearUsuario(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ObtenerIntentosFallidos(string dNI)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsuarioBE ObtenerPorUsuario(string usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetearIntentos(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SumarIntento(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
