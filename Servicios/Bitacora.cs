72fc45c7626ba6ec7e0b0163cc697589f426e347
Lautaro Martori <lautaromartori@gmail.com>
2026-05-28 21:10:56 -0300

    Bitacora con validaciones

--- Servicios/Bitacora.cs
+++ Servicios/Bitacora.cs
@@ -6,7 +6,10 @@
         public string Login { get; set; }
         public DateTime Fecha { get; set; }
         public string Modulo { get; set; }
-        public string Descripcion { get; set; }
+        public string Evento { get; set; }
         public int Criticidad { get; set; }
+
+        public string FechaSolo => Fecha.ToString("dd/MM/yyyy");
+        public string HoraSolo => Fecha.ToString("HH:mm");
     }
 }
