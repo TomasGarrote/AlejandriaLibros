a1d8351c6c9276c8c4110913fbbf41999faa29c2
Tomas Garrote <tomasgarrote123@gmail.com>
2026-06-26 08:50:33 -0300

    Centro la pos inicial de login

--- GUI/Login.Designer.cs
+++ GUI/Login.Designer.cs
@@ -183,6 +183,7 @@
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             this.panel1.ResumeLayout(false);
